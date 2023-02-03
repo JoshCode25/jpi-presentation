a730b15a88c45b278e1193833d8835b728ad2a70
JoshG <joshcode25@gmail.com>
2023-02-03 06:10:58 -0600

    installed gh-pages and sveltekit-gh-pages

--- gh-pages.js
+++ gh-pages.js
@@ -1,7 +1,7 @@
-var ghpages = require('gh-pages');
+import { publish } from 'gh-pages';
 
-ghpages.publish(
-	'public', // path to public directory
+publish(
+	'build', // path to public directory
 	{
 		branch: 'gh-pages',
 		repo: 'https://github.com/joshcode25/jpi-presentation.git', // Update to point to your repository
@@ -9,6 +9,7 @@ ghpages.publish(
 			name: 'Josh Gannon', // update to use your name
 			email: 'joshcode25@gmail.com', // Update to use your email
 		},
+		dotfiles: true,
 	},
 	() => {
 		console.log('Deploy Complete!');
